1810054f77e8ef172150332a69899408b58e1f68
AzimjonNajmiddinov <azim.najmiddinov97@gmail.com>
2026-03-22 04:57:06 +0500

    Make dub audio track DEFAULT=YES so player auto-selects it
    Player was starting on Russian audio. User had to manually switch
    to Uzbek dub, by which time the intro/music was already past,
    making it seem like there was no background audio.

--- app/Http/Controllers/InstantDubController.php
+++ app/Http/Controllers/InstantDubController.php
@@ -381,9 +381,9 @@ class InstantDubController extends Controller
             // Inject dub audio + subtitle tracks before the first STREAM-INF
             if (!$injected && str_starts_with($trimmed, '#EXT-X-STREAM-INF')) {
                 if (!$existingAudioGroup) {
-                    $output[] = "#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID=\"{$groupId}\",NAME=\"Original\",DEFAULT=YES,AUTOSELECT=YES";
+                    $output[] = "#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID=\"{$groupId}\",NAME=\"Original\",DEFAULT=NO,AUTOSELECT=NO";
                 }
-                $output[] = "#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID=\"{$groupId}\",NAME=\"{$dubName}\",LANGUAGE=\"{$lang}\",URI=\"dub-audio.m3u8\",DEFAULT=NO,AUTOSELECT=NO";
+                $output[] = "#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID=\"{$groupId}\",NAME=\"{$dubName}\",LANGUAGE=\"{$lang}\",URI=\"dub-audio.m3u8\",DEFAULT=YES,AUTOSELECT=YES";
                 $output[] = "#EXT-X-MEDIA:TYPE=SUBTITLES,GROUP-ID=\"{$subsGroupId}\",NAME=\"{$subName}\",LANGUAGE=\"{$lang}\",URI=\"dub-subtitles.m3u8\",DEFAULT=NO,AUTOSELECT=YES,FORCED=NO";
                 $injected = true;
             }
