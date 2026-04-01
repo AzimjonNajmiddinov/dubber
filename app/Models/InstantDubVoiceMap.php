0baf3436e845152d3cfface3dacb491993b238ac
AzimjonNajmiddinov <azim.najmiddinov97@gmail.com>
2026-04-01 17:32:29 +0500

    Fix InstantDubVoiceMap table name (singular vs plural mismatch)

--- app/Models/InstantDubVoiceMap.php
+++ app/Models/InstantDubVoiceMap.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class InstantDubVoiceMap extends Model
 {
+    protected $table = 'instant_dub_voice_map';
+
     protected $fillable = ['instant_dub_id', 'speaker_tag', 'voice_config'];
 
     protected $casts = ['voice_config' => 'array'];
