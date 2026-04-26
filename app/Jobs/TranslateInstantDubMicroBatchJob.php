e1fbf9a8ce67b929ca35f1c5e468715e5e376d5d
AzimjonNajmiddinov <azim.najmiddinov97@gmail.com>
2026-04-26 12:27:49 +0500

    Set tau=1.0 for force_voice: 100% reference voice, deterministic without prosody transfer

--- app/Jobs/TranslateInstantDubMicroBatchJob.php
+++ app/Jobs/TranslateInstantDubMicroBatchJob.php
@@ -314,7 +314,7 @@ class TranslateInstantDubMicroBatchJob implements ShouldQueue
             $lock->block(5, function () use ($voiceKey, $speakers, $forceVoice, $gender) {
                 $voiceMap = json_decode(Redis::get($voiceKey) ?? '{}', true) ?: [];
                 foreach (array_keys($speakers) as $tag) {
-                    $voiceMap[$tag] = ['driver' => 'mms', 'gender' => $gender, 'pool_name' => $forceVoice, 'tau' => 0.0];
+                    $voiceMap[$tag] = ['driver' => 'mms', 'gender' => $gender, 'pool_name' => $forceVoice, 'tau' => 1.0];
                 }
                 Redis::setex($voiceKey, 50400, json_encode($voiceMap));
             });
